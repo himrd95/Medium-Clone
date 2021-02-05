6579810391932af61d5cfea0a0aeb4845271dde6
Himanshu <himrd95@gmail.com>
2021-02-05 11:39:24 +0530

    added Blog Preview

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,9 +1,11 @@
 import { createStore, combineReducers, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
+// import { blogReducer } from "./blog/reducer";
 // import { searchReducer } from "./search/reducer";
 // import { reducer } from "./signup/reducer";
 // import { trendReducer } from "./trend/reducer";
 const rootReducer = combineReducers({
+  // blog: blogReducer
 //   signup: reducer,
 //   trend: trendReducer,
 //   search: searchReducer,
